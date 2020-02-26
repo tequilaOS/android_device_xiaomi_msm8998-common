d8086656620a1400f220f5e88b9ec7f2e247eaab
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    msm8998-common: Build and enable fastbootd mode
    Change-Id: I7f4246f3f583dcae7b3bfc0d3ae1cf4d577632cb
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- msm8998.mk
+++ msm8998.mk
@@ -194,6 +194,10 @@ PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
