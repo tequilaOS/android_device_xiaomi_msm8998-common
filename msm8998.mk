031847382dba1a4d127d97c5beee708a2cccf2f7
x0x00044 <zlewchan@outlook.com>
2023-11-02 22:57:45 +0100

    msm8998-common: Time for tequila!
    Change-Id: I9a683cc4f5f018e3aa389c56b47d818e9f16225b

--- msm8998.mk
+++ msm8998.mk
@@ -257,14 +257,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_msm8998
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -402,10 +394,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libdng_sdk.vendor_32 \
     libstdc++.vendor
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # Wifi
 PRODUCT_PACKAGES += \
